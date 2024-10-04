cdd900fe32666bdc1d697de90ea8e26ff0b2d464
anurukshi <amk1001862@student.hamk.fi>
2024-10-04 11:06:22 +0300

    Initial commit

---
+++ version control system/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Printer app");
+        System.out.println("This app is made by");
+        String Creator ="Anurukshi Thilakasiri";
+        System.out.println(Creator);
+        System.out.println("This app is made by "+Creator);
+
+    }
+}
